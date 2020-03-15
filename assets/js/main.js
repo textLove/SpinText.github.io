669dc3a787c7de406d2a4293f7d9bcf1f5ff99b6
nivendhak <nivendha.kannan@imaginea.com>
2020-03-15 12:02:19 +0530

    added UI changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,57 +162,57 @@ $(document).ready(function($) {
 	slickSliders();
 
 	// navigation
-	var OnePageNav = function() {
-		var navToggler = $('.navbar-toggler');
-		$(".smoothscroll[href^='#'], #probootstrap-navbar ul li a[href^='#']").on('click', function(e) {
-		 	e.preventDefault();
-		 	var hash = this.hash;
+	// var OnePageNav = function() {
+	// 	var navToggler = $('.navbar-toggler');
+	// 	$(".smoothscroll[href^='#'], #probootstrap-navbar ul li a[href^='#']").on('click', function(e) {
+	// 	 	e.preventDefault();
+	// 	 	var hash = this.hash;
 		 		
-		 	$('html, body').animate({
+	// 	 	$('html, body').animate({
 
-		    scrollTop: $(hash).offset().top
-		  }, 700, 'easeInOutExpo', function(){
-		    window.location.hash = hash;
-		  });
-		});
-		$("#probootstrap-navbar ul li a[href^='#']").on('click', function(e){
-			if ( navToggler.is(':visible') ) {
-		  	navToggler.click();
-		  }
-		});
+	// 	    scrollTop: $(hash).offset().top
+	// 	  }, 700, 'easeInOutExpo', function(){
+	// 	    window.location.hash = hash;
+	// 	  });
+	// 	});
+	// 	$("#probootstrap-navbar ul li a[href^='#']").on('click', function(e){
+	// 		if ( navToggler.is(':visible') ) {
+	// 	  	navToggler.click();
+	// 	  }
+	// 	});
 
-		$('body').on('activate.bs.scrollspy', function () {
-		  console.log('nice');
-		})
-	};
-	OnePageNav();
+	// 	$('body').on('activate.bs.scrollspy', function () {
+	// 	  console.log('nice');
+	// 	})
+	// };
+	// OnePageNav();
 
-	var offCanvasNav = function() {
-		var toggleNav = $('.js-pb_nav-toggle'),
-				offcanvasNav = $('.js-pb_offcanvas-nav_v1');
-		if( toggleNav.length > 0 ) {
-			toggleNav.click(function(e){
-				$(this).toggleClass('active');
-				offcanvasNav.addClass('active');
-				e.preventDefault();
-			});
-		}
-		offcanvasNav.click(function(e){
-			if (offcanvasNav.hasClass('active')) {
-				offcanvasNav.removeClass('active');
-				toggleNav.removeClass('active');
-			}
-			e.preventDefault();
-		})
-	};
-	offCanvasNav();
+	// var offCanvasNav = function() {
+	// 	var toggleNav = $('.js-pb_nav-toggle'),
+	// 			offcanvasNav = $('.js-pb_offcanvas-nav_v1');
+	// 	if( toggleNav.length > 0 ) {
+	// 		toggleNav.click(function(e){
+	// 			$(this).toggleClass('active');
+	// 			offcanvasNav.addClass('active');
+	// 			e.preventDefault();
+	// 		});
+	// 	}
+	// 	offcanvasNav.click(function(e){
+	// 		if (offcanvasNav.hasClass('active')) {
+	// 			offcanvasNav.removeClass('active');
+	// 			toggleNav.removeClass('active');
+	// 		}
+	// 		e.preventDefault();
+	// 	})
+	// };
+	// offCanvasNav();
 
-	var ytpPlayer = function() {
-		if ($('.ytp_player').length > 0) { 
-			$('.ytp_player').mb_YTPlayer();	
-		}
-	}
-	ytpPlayer();
+	// var ytpPlayer = function() {
+	// 	if ($('.ytp_player').length > 0) { 
+	// 		$('.ytp_player').mb_YTPlayer();	
+	// 	}
+	// }
+	// ytpPlayer();
 
 
 	
--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,19 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-const fs = require('fs');
-const nodemailer = require('nodemailer');
 
 admin.initializeApp();
 
-const gmailEmail = "nivendhawar2@gmail.com";
-const gmailPassword = "";
 
+var UpdateUserData = functions.database.ref('/users/{userID}')
+    .onCreate((snapshot, context) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const data = snapshot.val();
+        console.log('Setinglimit for', context.params.userID);
 
-var htmlmail = fs.readFileSync("index.html", "utf-8").toString();
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to the Firebase Realtime Database.
+        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+        return snapshot.ref.parent.child(context.params.userID + '/usage').set('0');
+    });
 
-var sendWelcomeEmail = functions.auth.user().onCreate(async (user) => {
-    const recipent_email = user.email;
-    const name = user.displayName;
-    console.log('New User ==',recipent_email);
-    try {
-
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: gmailEmail,
-                pass: gmailPassword,
-            },
-        });
-        let info = await transporter.sendMail({
-            from: '"nivendhawar2" <nivendhawar2@gmail.com>',
-            to: "knivendha@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>" // html body
-        });
-        console.log("Message sent: %s", info.messageId);
-
-
-
-    } catch (error) {
-        console.error('There was an error while sending the email:', error);
-    }
-    return null;
-});
-
-exports.sendWelcomeEmail = sendWelcomeEmail;
+exports.UpdateUserData = UpdateUserData;
